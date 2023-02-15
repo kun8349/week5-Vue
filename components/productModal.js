f6604b699a5998c3e65d100477962345f2a77ed4
Kevin <a039386773@gmial.com>
2023-02-15 23:21:07 +0800

    update2/15

---
+++ components/productModal.js
@@ -0,0 +1,62 @@
+export default {
+    props:['tempProduct','addProduct','qty'],
+    data(){
+        return{
+
+        }
+    },
+    template:`<div class="modal-dialog modal-xl">
+    <div class="modal-content">
+      <div class="modal-header bg-secondary text-white">
+        <h5 class="modal-title" id="staticBackdropLabel">
+          Modal title
+        </h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="row">
+          <div class="col-md-6">
+            <img class="img-fluid" :src="tempProduct.imageUrl" alt="singlePic">
+          </div>
+          <div class="col-md-6 d-flex flex-column justify-content-between">
+            <div>
+              <p class="badge bg-primary fs-5">
+                {{ tempProduct.category }}
+              </p>
+              <p>
+                商品描述：{{ tempProduct.content }}
+              </p>
+              <p>
+                商品內容：{{ tempProduct.description }}
+              </p>
+              <div class="h5" v-if="!tempProduct.origin_price">
+                {{ tempProduct.price }} 元
+              </div>
+              <del class="h6" v-else-if="tempProduct.origin_price">
+                原價 {{ tempProduct.origin_price }} 元
+              </del>
+              <div class="h5">
+                現在只要 {{ tempProduct.price }} 元
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-6"></div>
+              <div class="col-6">
+                <div class="input-group text-end mt-5">
+                  <input type="number" min="0" class="form-control w-50"
+                  v-model="qty">
+                  <button class="btn btn-primary" type="button" @click="addProduct(tempProduct,qty)">
+                    加入購物車
+                  </button>
+                </div>
+              </div>
+            </div>
+            
+            
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  </div>`,
+}
---
+++ js/index.js
@@ -0,0 +1,163 @@
+import productModal from "../components/productModal.js";
+const {createApp} = Vue;
+let cartModal = null;
+
+
+
+Object.keys(VeeValidateRules).forEach(rule => {
+    if (rule !== 'default') {
+      VeeValidate.defineRule(rule, VeeValidateRules[rule]);
+    }
+  });
+
+VeeValidateI18n.loadLocaleFromURL("./js/zh_TW.json");
+
+// Activate the locale
+VeeValidate.configure({
+generateMessage: VeeValidateI18n.localize('zh_TW'),
+validateOnInput: true, // 調整為：輸入文字時，就立即進行驗證
+});  
+const app = createApp({
+    
+    data(){
+        return{
+            products:[],
+            tempProduct:{},
+            cartList:[],
+            total:0,
+            qty:1,
+            form: {
+                user: {
+                  name: '',
+                  email: '',
+                  tel: '',
+                  address: '',
+                },
+                message: '',
+              },
+            loadingItem:'',
+        }
+    },
+    components:{
+        productModal,
+    },
+    methods:{
+        getProduct(){
+            axios.get(`${url}api/${path}/products`)
+                .then(res=>{
+                    this.products = res.data.products;
+                })
+                .catch(err=>{
+                    alert(err.response.data.message);
+                })
+        },
+        openModal(item){
+            this.tempProduct = {...item};
+            this.loadingItem = this.tempProduct.id;
+            axios.get(`${url}api/${path}/product/${this.tempProduct.id}`)
+                .then(res=>{
+                    cartModal.show();
+                    this.loadingItem = '';
+                })
+                .catch(err=>{
+                    alert(err.response.data.message);
+                })
+            
+        }, 
+        addProduct(item,qty=1){
+            const data = {
+                "product_id": item.id,
+                qty,
+            };
+            this.loadingItem = item.id
+            axios.post(`${url}api/${path}/cart`,{data})
+                .then(res=>{
+                    this.getCartList();
+                    cartModal.hide();
+                    this.loadingItem = '';
+                })
+                .catch(err=>{
+                    alert(err.response.data.message);
+                })
+        },
+        getCartList(){
+            axios.get(`${url}api/${path}/cart`)
+                .then(res=>{ 
+                    this.cartList = res.data.data.carts;
+                    this.total = res.data.data.total;
+                })
+                .catch(err=>{
+                    alert(err.response.data.message);
+                })
+        },
+        deleteAll(){
+            axios.delete(`${url}api/${path}/carts`)
+                .then(res=>{
+                    this.loading();
+                    this.getCartList();
+                })
+                .catch(err=>{
+                    alert(err.response.data.message);
+                })
+        },
+        deleteSingle(product){
+            this.loadingItem = product.id;
+            axios.delete(`${url}api/${path}/cart/${product.id}`)
+                .then(res=>{
+                    this.loading();
+                    this.getCartList();
+                    this.loadingItem = '';
+                })
+                .catch(err=>{
+                    alert(err.response.data.message);
+                })
+        },
+        editNum(product){
+            const data = {
+                product_id: product.id,
+                qty: product.qty
+            };
+            this.loadingItem = product.id;
+            axios.put(`${url}api/${path}/cart/${product.id}`,{data})
+                .then(res=>{
+                    this.getCartList();
+                    this.loading();
+                    this.loadingItem='';
+                })
+                .catch(err=>{
+                    alert(err.response.data.message);
+                })
+        },
+        order(){
+            axios.post(`${url}api/${path}/order`,{data:this.form})
+                .then(res=>{
+                    this.loading();
+                })
+                .catch(err=>{
+                    alert(err.response.data.message);
+                })
+        },
+        loading(){
+            let loader = this.$loading.show({});
+        // simulate AJAX
+            setTimeout(() => {
+                loader.hide()
+            }, 1000)
+        },
+
+    },
+    mounted(){
+        this.loading();
+        this.getProduct();
+        this.getCartList();
+        
+        cartModal = new bootstrap.Modal(document.getElementById('cartModal'), {
+            keyboard: false
+        })
+    },
+});
+app.use(VueLoading.LoadingPlugin);
+app.component('VForm', VeeValidate.Form);
+app.component('VField', VeeValidate.Field);
+app.component('ErrorMessage', VeeValidate.ErrorMessage);
+app.mount('#app');
